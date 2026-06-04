67d1d006cec285807c5f306711c4d7836e77cbee
Joshuaburle <11491@holbertonschool.com>
2026-06-04 14:27:06 +0200

    fixing code

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,27 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
 	dlistint_t *tmp;
-	unsigned int p;
+	unsigned int i;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	saved_head = *head;
-	p = 0;
-
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-
-	if (*head == NULL)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-
 	tmp = *head;
 
-	if (index == 0)
-	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-	}
-	else
-	{
+	for (i = 0; tmp != NULL && i < index; i++)
+		tmp = tmp->next;
+
+	if (tmp == NULL)
+		return (-1);
+
+	if (tmp->prev != NULL)
 		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-		*head = saved_head;
-	}
+	else
+		*head = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
 
 	free(tmp);
 	return (1);
